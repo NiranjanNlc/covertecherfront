72ba018d504f34ede0679389e6003817ce9f9929
NiranjanNlc <niranjannlc10@gmail.com>
2020-01-08 12:27:56 +0545

    Creation of the teacher component

---
+++ src/Teacher.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+
+function Teacher() {
+    return (
+      <div   >
+            <div style={{"float":'left'}}><img   width="150" height="150" src="https://www.fillmurray.com/200/100"/></div>
+            <div  style={{"float":'left'}}>
+                Name:<b>Niranjan</b><br></br>
+                Locatin:<b>kathmandu</b><br></br>
+                Time:<b>Day</b><br></br>
+                <button>Book</button>
+    </div>
+      </div>
+    );
+  }
+  
+  export default Teacher;
