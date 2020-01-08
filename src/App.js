e012de0117533d2a123e944f86068b5b335c8282
NiranjanNlc <niranjannlc10@gmail.com>
2020-01-08 12:41:46 +0545

    folder added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Teacher from './teacher/Teacher.js'
 
 function App() {
   return (
     <div className="App">
-     
+      <Teacher></Teacher>
     </div>
   );
 }
--- src/teacher/Teacher.js
+++ src/teacher/Teacher.js
@@ -5,7 +5,7 @@ import React from 'react'
 function Teacher() {
     return (
       <div   >
-            <div style={{"float":'left'}}><img   width="150" height="150" src="https://www.fillmurray.com/200/100"/></div>
+            <div style={{"float":'left'}}><img   width="150" height="150" src="/teacher.jpg"/></div>
             <div  style={{"float":'left'}}>
                 Name:<b>Niranjan</b><br></br>
                 Locatin:<b>kathmandu</b><br></br>
