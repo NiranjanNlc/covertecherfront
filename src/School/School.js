8125a8e4997641227ea66e63c6c94535f9317080
NiranjanNlc <niranjannlc10@gmail.com>
2020-01-10 12:34:57 +0545

    starting working on schoolss

--- src/School/School.js
+++ src/School/School.js
@@ -2,18 +2,20 @@ import React from 'react'
 
 
 
-function School() {
-    return (
-      <div   >
-            <div style={{"float":'left'}}><img   width="150" height="150" src="/teacher.jpg"/></div>
-            <div  style={{"float":'left'}}>
-                Id:<b>1</b><br></br>
-                Name:<b>Mind Light </b><br></br>
-                Locatin:<b>kathmandu</b><br></br>
-                <button>Send Request</button>
-    </div>
-      </div>
-    );
-  }
+
+
+// function School() {
+//     return (
+//       <div   >
+//             <div style={{"float":'left'}}><img   width="150" height="150" src="/teacher.jpg"/></div>
+//             <div  style={{"float":'left'}}>
+//                 Id:<b>1</b><br></br>
+//                 Name:<b>Mind Light </b><br></br>
+//                 Locatin:<b>kathmandu</b><br></br>
+//                 <button>Send Request</button>
+//     </div>
+//       </div>
+//     );
+//   }
   
-  export default School;
+//   export default School;
--- src/teacher/Teacher.js
+++ src/teacher/Teacher.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import TeacherService from './TeacherService.js'
-<<<<<<< HEAD
  
 class Teacher extends React.Component {
   constructor() {
@@ -54,70 +53,10 @@ class Teacher extends React.Component {
                  Time:<b>Day</b><br></br>
                  <button>Book</button> */}
      </div>
+    )
+                }
+                    
+            }
+export default Teacher 
 
-=======
-
-
-// function Teacher() {
-//     return (
-//       <div   >
-//             <div style={{"float":'left'}}><img   width="150" height="150" src="/teacher.jpg"/></div>
-//             <div  style={{"float":'left'}}>
-//                 Name:<b>Niranjan</b><br></br>
-//                 Locatin:<b>kathmandu</b><br></br>
-//                 Subject:<b>English ,math</b><br></br>
-//                 Time:<b>Day</b><br></br>
-//                 <button>Book</button>
-//     </div>
-//       </div>
-//     );
-//   }
-  
-//   export default Teacher;
-
-class Teacher extends React.Component {
-  constructor(props) {
-      super(props)
-      this.state = {
-          teacher: [],
-          message: null
-      }
-      this.refreshTeacher = this.refreshTeacher.bind(this)
-  }
-  componentDidMount() {
-      this.refreshTeacher();
-  }
-  refreshTeacher() {
-      TeacherService.retrieveAllTeacher()  
-          .then(
-              response => {
-                  console.log(response);
-                  this.setState({ teacher: response.data })
-              }
-          )
-  }
-  render()
-  {
-    return(
-      <div  >
-          <div style={{"float":'left'}}><img   width="150" height="150" src="/teacher.jpg"/></div>
-             <div  style={{"float":'left'}}>
-           
-                Name:<b>Niranjan</b><br></br>
-                 Locatin:<b>kathmandu</b><br></br>
-                 Subject:<b>English ,math</b><br></br>
-                 Time:<b>Day</b><br></br>
-                 <button>Book</button>
-               
-                 {/* Name:<b>Niranjan</b><br></br>
-                 Locatin:<b>kathmandu</b><br></br>
-                 Subject:<b>English ,math</b><br></br>
-                 Time:<b>Day</b><br></br>
-                 <button>Book</button> */}
-     </div>
-       </div>
->>>>>>> 98bab5c85754a0704030b23b0ea6e44189d2f358
-    );
-  }
-}
-export default Teacher;
+ 
